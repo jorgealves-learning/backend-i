c1017f4468073f91ef225fac024a8daa81b172d3
Jorge Alves <jorgeandrealves@gmail.com>
2025-03-20 16:49:31 +0000

    feat: project structure

--- fastapi_project/src/fastapi_project/main.py
+++ fastapi_project/src/fastapi_project/main.py
@@ -1,7 +1,7 @@
 from typing import Annotated
 from fastapi.responses import JSONResponse
-from session_12 import database
-from session_12.models import Task
+from fastapi_project import database
+from fastapi_project.models import Task
 from fastapi import FastAPI, Form, status
 from fastapi.exceptions import HTTPException
 
@@ -9,25 +9,18 @@ api = FastAPI(
     title="TODO Etic_Algarve API"
 )
 
-@api.get("/task",response_model=Task)
+@api.get("/task",status_code=status.HTTP_200_OK)
 def list_task():
-    pass
+    return database.get_all_tasks()
 
 @api.post("/task")
 def create_task(data: Annotated[Task,Form()]):
     if not data:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
-    database.create(task=Task(**data))
+    database.create_task(task=data)
     return JSONResponse({"status": "created"}, status_code=status.HTTP_201_CREATED)
 
-@api.put("/task")
-def edit_task():
-    pass
-
-@api.patch("/task")
-def close_task():
-    pass
-
-@api.delete("/task")
-def delete_task():
-    pass
+@api.delete("/task", status_code=status.HTTP_200_OK)
+def delete_task(title: str):
+    assert title 
+    database.delete_task(title=title)
